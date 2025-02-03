f32e6a309eb8b4eb6783967fd31148cd1d470126
Malte Åqvist <malte.aqvist@maurten.com>
2025-02-03 14:39:14 +0100

    removed vo2max

--- func_app.py
+++ func_app.py
@@ -27,11 +27,13 @@ def fuel_calculator(weight, distance, duration):
     input_json["data"]["weight"] = weight
     input_json["data"]["environment"]["raceData"]["distance"] = distance
     input_json["data"]["environment"]["raceData"]["duration"] = duration
+    print(f"input_json: {input_json}")
 
     try:
         response = requests.post(url, json=input_json, headers=headers)
         outputJson = response.json()
         status = response.status_code
+        print(f" version: {outputJson['version']}")
         if status == 200:
             output = ""
             warmup_product = outputJson["data"]["fuelingProtocol"]["warmUp"]["details"][0]["product"]
